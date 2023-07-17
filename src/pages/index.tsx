e331919b37454e2d7ad5572f3c4a46cd9c7ef012
tang <15977536060@163.com>
2023-07-17 12:01:51 +0800

    style: Implement tailwind css in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,14 @@
-import Head from 'next/head';
-import { Button } from 'primereact/button';
-import CatCard from '../components/cards/cat/CatCard';
-import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks';
-import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
-import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
+import CatCard from '@/components/cards/cat/CatCard';
+import { mockCatCardProps } from '@/components/cards/cat/CatCard.mocks';
+import PrimaryLayout from '@/components/layouts/primary/PrimaryLayout';
+import SidebarLayout from '@/components/layouts/sidebar/SidebarLayout';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
   return (
-    <>
-      <Head>
-        <title>index page</title>
-      </Head>
-      <section>
-        <CatCard {...mockCatCardProps.base} />
-
-        <Button label="Check" icon="pi pi-check" />
-      </section>
-    </>
+    <section className="w-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+      <CatCard {...mockCatCardProps.base} />
+    </section>
   );
 };
 
