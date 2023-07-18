37933a48abb9c35fc11fec8909de0d1396b9b5a6
tang <15977536060@163.com>
2023-07-18 11:24:17 +0800

    feat: Adding Search component and reusing styles in Tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import CatCard from '@/components/cards/cat/CatCard';
 import { mockCatCardProps } from '@/components/cards/cat/CatCard.mocks';
 import PrimaryLayout from '@/components/layouts/primary/PrimaryLayout';
 import SidebarLayout from '@/components/layouts/sidebar/SidebarLayout';
+import Search from '@/components/utility/base/Search';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
   return (
     <section className="w-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+      <Search />
       <CatCard {...mockCatCardProps.base} />
     </section>
   );
