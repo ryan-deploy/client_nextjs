93d1a92892276de3fbe60dd3f5281d487e054c3a
tang <15977536060@163.com>
2023-07-24 15:36:35 +0800

    build: Remove primereact library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,7 @@
 import type { AppProps } from 'next/app';
-import { PrimeReactProvider } from 'primereact/api';
 
 import { NextPageWithLayout } from './page';
 
-import 'primeicons/primeicons.css';
-import 'primereact/resources/primereact.min.css';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import './globals.css';
 
 interface AppPropsWithLayout extends AppProps {
@@ -16,11 +12,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
-  return (
-    <PrimeReactProvider>
-      {getLayout(<Component {...pageProps} />)}
-    </PrimeReactProvider>
-  );
+  return <div>{getLayout(<Component {...pageProps} />)}</div>;
 }
 
 export default MyApp;
