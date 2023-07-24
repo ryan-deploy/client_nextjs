ec959a5603e8d949d0a24b699221db9d8b692c26
tang <15977536060@163.com>
2023-07-24 18:06:07 +0800

    chore: Rename header to TopBar then move it to /top_bar/

--- src/components/layouts/primary/PrimaryLayout.tsx
+++ src/components/layouts/primary/PrimaryLayout.tsx
@@ -1,5 +1,5 @@
-import Header from '@/components/navigation/header/Header';
 import Footer from '@/components/navigation/tab_bar/TabBar';
+import Header from '@/components/navigation/top_bar/TopBar';
 import Head from 'next/head';
 
 export interface IPrimaryLayout extends React.ComponentPropsWithoutRef<'div'> {}
