8db9caa5fa67649e6f25d68b75d10e635e95b354
tang <15977536060@163.com>
2023-07-26 11:54:18 +0800

    refactor: To navigate between routes with "useRouter" hook

--- src/components/navigation/tab_bar/TabBar.tsx
+++ src/components/navigation/tab_bar/TabBar.tsx
@@ -1,14 +1,18 @@
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 import RestoreIcon from '@mui/icons-material/Restore';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 export interface ITabBar extends React.ComponentPropsWithoutRef<'div'> {}
 
 const TabBar: React.FC<ITabBar> = ({ className, ...divProps }) => {
-  const [value, setValue] = useState(0);
+  const router = useRouter();
+  console.log(router);
+  // By using this way to set default value for "value" state, it cause a issue that
+  //  when user input http://localhost:3000/about, the app shows about component in screen but BottomNavigation sets the "/" as value which should be "/about", and active the wrong button
+  const [value, setValue] = useState('/');
 
   return (
     <div
@@ -18,13 +22,21 @@ const TabBar: React.FC<ITabBar> = ({ className, ...divProps }) => {
       <BottomNavigation
         showLabels
         value={value}
-        onChange={(event, newValue) => {
+        onChange={(_, newValue) => {
+          router.replace(newValue);
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction
+          value="/"
+          label="Recents"
+          icon={<RestoreIcon />}
+        />
+        <BottomNavigationAction
+          value="/about"
+          label="Favorites"
+          icon={<FavoriteIcon />}
+        />
       </BottomNavigation>
     </div>
   );
